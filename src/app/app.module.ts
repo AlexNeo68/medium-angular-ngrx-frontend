d2f8e1dea9ae0e1c3cc5e2e303f6c93b5fcea738
Александр Неудахин <aleksandrneudahin@Aleksandrs-Air.localnet>
2023-04-05 07:44:16 +0300

    local

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
-import { NgModule, isDevMode } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { AuthModule } from 'src/app/auth/auth.module'
+import {NgModule, isDevMode} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import {AuthModule} from 'src/app/auth/auth.module'
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-import { StoreModule } from '@ngrx/store'
-import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import {AppRoutingModule} from './app-routing.module'
+import {AppComponent} from './app.component'
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+import {StoreModule} from '@ngrx/store'
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
 
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
-import { EffectsModule } from '@ngrx/effects'
-import { TopBarModule } from 'src/app/shared/modules/top-bar/top-bar.module'
-import { PersistanceService } from 'src/app/shared/services/persistance.service'
-import { AccessTokenInterceptor } from 'src/app/shared/interceptors/access-token.interceptor'
-import { GlobalFeedModule } from 'src/app/global-feed/global-feed.module'
-import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store'
-import { YourFeedModule } from 'src/app/your-feed/your-feed.module'
-import { TagFeedModule } from 'src/app/tag-feed/tag-feed.module'
-import { ArticleModule } from 'src/app/article/article.module'
-import { ArticleNewModule } from 'src/app/article-new/article-new.module'
-import { ArticleEditModule } from 'src/app/article-edit/article-edit.module'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
+import {EffectsModule} from '@ngrx/effects'
+import {TopBarModule} from 'src/app/shared/modules/top-bar/top-bar.module'
+import {PersistanceService} from 'src/app/shared/services/persistance.service'
+import {AccessTokenInterceptor} from 'src/app/shared/interceptors/access-token.interceptor'
+import {GlobalFeedModule} from 'src/app/global-feed/global-feed.module'
+import {routerReducer, StoreRouterConnectingModule} from '@ngrx/router-store'
+import {YourFeedModule} from 'src/app/your-feed/your-feed.module'
+import {TagFeedModule} from 'src/app/tag-feed/tag-feed.module'
+import {ArticleModule} from 'src/app/article/article.module'
+import {ArticleNewModule} from 'src/app/article-new/article-new.module'
+import {ArticleEditModule} from 'src/app/article-edit/article-edit.module'
+import {SettingsModule} from 'src/app/settings/settings.module'
 
 @NgModule({
   declarations: [AppComponent],
@@ -29,8 +30,8 @@ import { ArticleEditModule } from 'src/app/article-edit/article-edit.module'
     AuthModule,
     NgbModule,
     HttpClientModule,
-    StoreModule.forRoot({ router: routerReducer }, {}),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    StoreModule.forRoot({router: routerReducer}, {}),
+    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: !isDevMode()}),
     EffectsModule.forRoot([]),
     TopBarModule,
     GlobalFeedModule,
@@ -39,6 +40,7 @@ import { ArticleEditModule } from 'src/app/article-edit/article-edit.module'
     ArticleNewModule,
     ArticleEditModule,
     ArticleModule,
+    SettingsModule,
     StoreRouterConnectingModule.forRoot(),
   ],
   providers: [
@@ -51,4 +53,4 @@ import { ArticleEditModule } from 'src/app/article-edit/article-edit.module'
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
